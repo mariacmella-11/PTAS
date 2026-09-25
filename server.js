4f08726f48eaee67849b41166745caab055be425
mariacmella-11 <teytis449@gmail.com>
2026-09-25 03:12:38 +0000

    edit: Criei uma rota que retorna um array fixo com 3 tarefas

--- server.js
+++ server.js
@@ -1,9 +1,19 @@
-const express = require('express')
+import express from 'express';
 
 const app = express();
 
+const tarefas = [
+    {id: 1, titulo:'Estudar ptas', concluida: false},
+    {id: 2, titulo: 'Fazer atividades', concluida: true},
+    {id: 3, titulo: 'Ler documento', concluida: false}
+]
+
 app.get('/', (req,res) => {
     res.send('API de tarefas no ar')
+})
+
+app.get('/tarefas', (req, res) => {
+    res.json(tarefas)
 })
 
 app.listen(3000, () => {
